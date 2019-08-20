75a00e9193e6c7281ca49eb0f89c06686efe673f
APCS3rd <APCS3rd@MHS-LT9-22>
2019-08-20 10:12:31 -0700

    Complete hello world project

---
+++ Hello World Project/src/HelloWorld.java
@@ -0,0 +1,11 @@
+//Alice Peterson
+//August 16, 2019
+//First class (iconic) in Java for APCS A
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+	}
+
+}
