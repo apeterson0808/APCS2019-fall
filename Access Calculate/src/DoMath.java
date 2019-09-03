351a2047af5ab97592f7826461d358aeb455db04
APCS3rd <APCS3rd@MHS-LT9-22>
2019-09-03 09:53:50 -0700

    basis of average1 written

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,12 @@
+//Alice Peterson
+//August 29,2019
+//Calls math functions from calculate library
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(2));
+		System.out.println(Calculate.cube(2));
+		
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,23 @@
+//Alice Peterson
+//August 29, 2019
+//Library of math functions
+public class Calculate {
+//a call to square, returns the square of the value passed
+	public static int square(int number) {
+		int answer;
+		answer = number*number;
+		return answer;
+	}
+//a call to cube, returns the cube value passed
+	public static int cube(int number) {
+		int answer;
+		answer = number*number*number;
+		return answer;
+	}
+//a call to average, returns the average of the values passed, overload method
+	public static double average(double number) {
+		double answer;
+		answer = 
+	}
+}
+
