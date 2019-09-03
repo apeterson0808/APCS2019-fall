c5eb134b75d5575333e379bbc96dadb074723a95
APCS3rd <APCS3rd@MHS-LT9-22>
2019-09-03 10:28:15 -0700

    finished discriminant

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -6,7 +6,11 @@ public class DoMath {
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(2));
 		System.out.println(Calculate.cube(2));
-		
+		System.out.println(Calculate.average(2,4.5));
+	    System.out.println(Calculate.average(1,3,2));
+	    System.out.println(Calculate.toDegrees(1));
+	    System.out.println(Calculate.toRaidian(1));
+	    System.out.println(Calculate.discriminant(6,10,-1));
 	}
 
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -15,9 +15,35 @@ public class Calculate {
 		return answer;
 	}
 //a call to average, returns the average of the values passed, overload method
-	public static double average(double number) {
+	public static double average(double number1,double number2) {
 		double answer;
-		answer = 
+		answer = (number1*number2)/2;
+		return answer;
 	}
+//a call to average, returns the average of the three values passed, overload method
+	public static double average(double number1, double number2, double number3) {
+		double answer;
+		answer = (number1*number2*number3)/3;
+		return answer;
+	}
+//a call to toDegrees, converts an angle measure from radians into degrees
+	public static double toDegrees(double number) {
+		double answer;
+		answer = number*(180/3.14159);
+		return answer;
+	}
+//a call to toRadian, converts an angle measure from degrees into radians
+	public static double toRaidian(double number) {
+		double answer;
+		answer = number*(3.14159/180);
+		return answer;
+	}
+//a call to discriminant, provides coefficients of a quadratic equation in standard form and returns discriminant
+	public static double discriminant(double number1, double number2, double number3) {
+		double answer;
+		answer = (number2*number2)-(4*number1*number3);
+		return answer;
+	}
+//a call to toImproperFrac, 
 }
 
