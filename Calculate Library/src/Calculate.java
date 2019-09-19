79ef7bfd20e958bb822dfc4194fe4c16c090ba5a
APCS3rd <APCS3rd@MHS-LT9-22>
2019-09-19 10:52:40 -0700

    ahhh ok pls finish tonite

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,27 +4,27 @@
 public class Calculate {
 //Part 1
 //a call to square, returns the square of the value passed
-	public static int square(int number) {
+	public static int square(int a) {
 		int answer;
-		answer = number*number;
+		answer = a*a;
 		return answer;
 	}
 //a call to cube, returns the cube value passed
-	public static int cube(int number) {
+	public static int cube(int a) {
 		int answer;
-		answer = number*number*number;
+		answer = a*a*a;
 		return answer;
 	}
 //a call to average, returns the average of the values passed, overload method
-	public static double average(double number1,double number2) {
+	public static double average(double a,double b) {
 		double answer;
-		answer = (number1*number2)/2;
+		answer = (a*b)/2;
 		return answer;
 	}
 //a call to average, returns the average of the three values passed, overload method
-	public static double average(double number1, double number2, double number3) {
+	public static double average(double a, double b, double c) {
 		double answer;
-		answer = (number1*number2*number3)/3;
+		answer = (a*b*c)/3;
 		return answer;
 	}
 //a call to toDegrees, converts an angle measure from radians into degrees
@@ -34,21 +34,21 @@ public class Calculate {
 		return answer;
 	}
 //a call to toRadian, converts an angle measure from degrees into radians
-	public static double toRaidian(double number) {
+	public static double toRaidian(double a) {
 		double answer;
-		answer = number*(3.14159/180);
+		answer = a*(3.14159/180);
 		return answer;
 	}
 //a call to discriminant, provides coefficients of a quadratic equation in standard form and returns discriminant
-	public static double discriminant(double number1, double number2, double number3) {
+	public static double discriminant(double a, double b, double c) {
 		double answer;
-		answer = (number2*number2)-(4*number1*number3);
+		answer = (b*b)-(4*a*c);
 		return answer;
 	}
 //a call to toImproperFrac, converts mixed number into an improper frac
 	public static void toImproperFrac (int whole, int numer, int denom) { 
 		int answer = (denom*whole)+numer;
-		System.out.println(answer"//"denom);
+		System.out.println(answer"/"denom);
 	}
 //a call toMixedNum, converts a improper frac into a mixed number
 	public static void toMixedNum (int numer, int denom) {
@@ -56,13 +56,13 @@ public class Calculate {
 		System.out.println(answer%denom);
 	}
 //a call to foil, converts binomial multiplication of the form (ax+b)(cx+d) into a quadratic equation
-	public static void foil (int a, int b, int c, int d){
+	public static void foil (int a, int b, int c, int d) {
 		
 	}
 
 //Part 2 
 //a call to isDivisibleBy, determine whether or not one integer is evenly divisible by another
-	public static boolean toDivisibleBy (int a,int b){
+	public static boolean toDivisibleBy (int a,int b) {
 		boolean divis = false;
 		if(a%b==0) {
 			divis=true;
@@ -70,7 +70,8 @@ public class Calculate {
 		return divis;}
 		
 //a call to absValue, returns the abolute number of the values passed
-		public static double absValue (double a){
+		public static double absValue (double a) {
+			if(a) {
 			
 		}
 //a call to max, returns the larger of two values passed, overload
@@ -93,11 +94,15 @@ public class Calculate {
 				return b; 
 			}
 		}
-//a call to round2
+//a call to round2, rounds variable correctly to two decimal places
+	public static double round2 (double a) {
+		(a*100);
 	}
+}
+	
 
 //Part 3
 //a call to exponent, raises value of a positive integer power
-
+		
 	
 
