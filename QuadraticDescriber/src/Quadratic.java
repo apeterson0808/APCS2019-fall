42b0ad5d3e3bee18ca5c4d6070c2afcb3b935ac5
APCS3rd <APCS3rd@MHS-LT9-22>
2019-09-24 11:03:14 -0700

    Begun new project

---
+++ QuadraticDescriber/src/Quadratic.java
@@ -0,0 +1,9 @@
+//Alice Peterson
+//September 23, 2019
+//Runs the calculations for QuadraticClient
+public class Quadratic {
+	public static String quadrDescriber (double a, double b, double c) {
+		
+		return "";
+	}
+}
---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,17 @@
+//Alice Peterson
+//September 23, 2019
+//Will print out the string quadrDescriber returns and asks user if they want to input again
+public class QuadraticClient {
+	public static void main(String[] args) {
+	//set up the scanner
+		
+	int a=0;
+	int b=0;
+	int c=0;
+	
+	String description = Quadratic.quarDescriber (a,b,c);
+	
+	System.out.println(description);
+	//ask if they want to print again
+	}
+}
