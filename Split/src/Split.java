eefe86861407b19068b9c49552401d61df5d1f1a
APCS3rd <APCS3rd@MHS-LT9-22>
2019-10-25 10:53:31 -0700

    skskskskl

--- Split/src/Split.java
+++ Split/src/Split.java
@@ -1,6 +1,20 @@
 //Alice Peterson
 //October 22, 2019
 //Split String project
+
+/*Split on bread
+Is there bread?
+	(get the index of bread)
+	Make a substring after
+		1st bread
+	Is there bread?
+		Return [i]
+	Else
+		Not a sandwich
+Else
+	Not a sandwich
+*/
+
 import java.util.*;
 
 public class Split {
@@ -11,61 +25,35 @@ public class Split {
 		"I reallyreally likeapples".split("really");
 		System.out.println();
 // Your task Part 0
-
 //String.split();
-
 //It's a method that acts on a string, <StringName>.split(<sp>);
-
 //Where sp is the string where the string splits
-
 //And it returns an array
-
 // Example: "I like apples!".split(" ");
-
 // it will split at spaces and return an array of ["I","like","apples!"]
-
 // Example 2: "I really like really red apples!".split("really")
-
 // it will split at the word "really" and return an array of ["I "," like ","red apples!"]
-
 //play around with String.split!
-
 //What happens if you "I reallyreally likeapples".split("really") ?
 
 //Your task Part 1:
-
 /*Write a method that take in a string like
-
 * "applespineapplesbreadlettucetomatobaconmayohambreadcheese"
-
 * describing a sandwich.
-
 * Use String.split to split up the sandwich by the word "bread" and return what's in the middle of
-
 * the sandwich and ignores what's on the outside
-
 * What if it's a fancy sandwich with multiple pieces of bread?
-
 */
 
 //Your task pt 2:
-
 /*Write a method that take in a string like
-
 * "apples pineapples bread lettuce tomato bacon mayo ham bread cheese"
-
 * describing a sandwich
-
 * use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of
-
 * the sandwich and ignores what's on the outside.
-
 * Again, what if it's a fancy sandwich with multiple pieces of bread?
-
 */
 
  }
 
 }
-
-
