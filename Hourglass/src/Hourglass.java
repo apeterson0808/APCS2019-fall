54b8e1c0f9563c1414f87bbae8da0b4296341b8f
APCS3rd <APCS3rd@MHS-LT9-22>
2019-10-25 10:26:32 -0700

    nyohohoh

--- Hourglass/src/Hourglass.java
+++ Hourglass/src/Hourglass.java
@@ -1,6 +1,6 @@
 //Alice Peterson
 //October 8, 2019
-//Prints out hourglass, can change size of hourglass
+//Prints out hourglass, can change size porpotionally 
 public class Hourglass {
 	public static void main(String[] args){
 	System.out.print("|");
@@ -9,14 +9,14 @@ public class Hourglass {
 	}
 	System.out.print("|");
 	for (int row=1; row <= 4; row++) {
-	//make below method
+	
 		printSpaces(int row) {
 			for (int space=1; printSpaces=row; printSpaces++);
 			System.out.print(" ");
 			}
 		}
 	System.out.print ("\\");
-	//make below method pls
+	
 	printColons(-2*row+10);
 	
 	System.out.println("/");
