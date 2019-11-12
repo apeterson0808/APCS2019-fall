53ffaf54bcf679ec6de0b66484a8e153d552dd30
APCS3rd <APCS3rd@MHS-LT9-22>
2019-11-12 11:06:33 -0800

    ch2 done

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -8,9 +8,13 @@ public class FracCalc {
     public static void main(String[] args) 
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
-      	Scanner input = new Scanner(System.in);
-        String expression= input.nextLine();
-        System.out.println(produceAnswer(expression));
+    	//Receives input and has while loop to quit 
+    	Scanner input = new Scanner(System.in);
+        String expression = input.nextLine();
+        while(!expression.equals("quit")) {
+        	 System.out.println(produceAnswer(expression));
+        	 String expression2 = input.nextLine();
+        }
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -24,10 +28,32 @@ public class FracCalc {
 
     public static String produceAnswer(String input)
     { 
+    	//Stored values in arrays
+    	String[] val1 = {"0","0","1"};
+    	String[] val2 = {"0","0","1"};
+    	
+    	//Splitting whole expression to only second value
     	String[] splitEx = input.split(" ");
-    	return splitEx[2];
+    	//Splitting if mixed number
+    	if(splitEx[2].contains("_")) {
+    		String[] splitVal2 = splitEx[2].split("_");
+    		String[] splitFrac = splitVal2[1].split("/");
+    		val2[0]= splitVal2[0];
+    		val2[1]= splitFrac[0];
+    		val2[2]= splitFrac[1];
+    		}
+    	//Splitting if just fraction
+    	else if(splitEx[2].contains("/")) {
+    		String[] splitFrac = splitEx[2].split("/");
+    		val2[1]= splitFrac[0];
+    		val2[2]= splitFrac[1];}
+    	//Splitting if just whole number
+    	else {
+    		val2[0]= splitEx[2];
+    	}
+    	return "whole:"+val2[0]+" numerator:"+val2[1]+" denominator:"+val2[2];
     }
+ }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
     
-}
